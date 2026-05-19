155e5a296f1e9d9111cd35099acae4224b2f5382
Legesly <hdarko@byupathway.edu>
2026-05-19 18:17:25 -0500

    update

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal();
+        PromptGenerator generator = new PromptGenerator();
+
+        int choice = 0;
+
+        while (choice != 5)
+        {
+            Console.WriteLine("Please choose one of the following options:");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display the journal");
+            Console.WriteLine("3. Save the journal to a file");
+            Console.WriteLine("4. Load the journal from a file");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+
+            choice = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            if (choice == 1)
+            {
+                string prompt = generator.GetRandomPrompt();
+                Console.WriteLine(prompt);
+                Console.Write("> ");
+                string response = Console.ReadLine();
+
+                Entry entry = new Entry
+                {
+                    _date = DateTime.Now.ToShortDateString(),
+                    _prompt = prompt,
+                    _response = response
+                };
+
+                journal.AddEntry(entry);
+            }
+            else if (choice == 2)
+            {
+                journal.DisplayAll();
+            }
+            else if (choice == 3)
+            {
+                Console.Write("Enter filename: ");
+                string filename = Console.ReadLine();
+                journal.SaveToFile(filename);
+                Console.WriteLine("Journal saved.\n");
+            }
+            else if (choice == 4)
+            {
+                Console.Write("Enter filename: ");
+                string filename = Console.ReadLine();
+                journal.LoadFromFile(filename);
+                Console.WriteLine("Journal loaded.\n");
+            }
+        }
     }
 }
